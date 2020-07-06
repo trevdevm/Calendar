631c132766eb85a481b352ec2d849c85393aaf21
Trevor Munns <trevmunns@gmail.com>
2020-07-05 19:17:06 -0500

    post deploy

---
+++ cal-client/src/Components/App/App.js
@@ -0,0 +1,273 @@
+import React from "react";
+import { Router, navigate } from "@reach/router";
+import Calendar from "../Calendar/Calendar";
+import DayChosen from "../DayChosen/DayChosen";
+import Cthanks from "../Cthanks/Cthanks";
+import {
+  addDays,
+  startOfMonth,
+  endOfMonth,
+  addMonths,
+  subMonths,
+  startOfDay,
+  isSameMonth,
+  getDate,
+} from "date-fns";
+import axios from "axios";
+import { updateTime } from "../../helper";
+
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentMonth: new Date(),
+      selectedDate: new Date(),
+      times: ["9:00", "10:00", "11:00", "12:00", "1:00", "2:00", "3:00", "4:00", "5:00"],
+      error: false,
+      current: new Date(),
+      first: {},
+      second: {},
+      initTime: {},
+    };
+
+    this.getEm = this.getEm.bind(this);
+    this.updating = this.updating.bind(this);
+    this.onDateClick = this.onDateClick.bind(this);
+    this.nextMonth = this.nextMonth.bind(this);
+    this.prevMonth = this.prevMonth.bind(this);
+    this.delay = this.delay.bind(this);
+    this.initializeCal = this.initializeCal.bind(this);
+  }
+
+  componentDidMount() {
+    this.initializeCal();
+  }
+
+  async initializeCal() {
+    const currentMonth = this.state.currentMonth;
+    const startMonth = startOfMonth(currentMonth);
+    const endMonth = endOfMonth(addMonths(startMonth, 1));
+    const theStart = startMonth.toISOString();
+    const theEnd = endMonth.toISOString();
+
+    try {
+      let res = await axios.get(`http://localhost:3005/api/days/${theStart}/${theEnd}`);
+
+      const days = res.data.data;
+
+      const first = days.first;
+      const second = days.second;
+
+      this.setState({
+        first: first,
+        second: second,
+        initTime: first,
+      })
+
+      return;
+
+    } catch (err) {
+      if (err) {
+        console.log(err.response);
+      }
+      return;
+    }
+  }
+
+  async getEm(choice) {
+    const day = startOfDay(choice);
+    const nextday = startOfDay(addDays(day, 1));
+    const theDay = day.toISOString();
+    const theNextDay = nextday.toISOString();
+    try {
+      let res = await axios.get(
+        `http://localhost:3005/api/date/${theDay}/${theNextDay}`
+      );
+
+      if (res.length || res.data.success) {
+
+        let inComing = await res.data.data[0].time;
+
+        this.setState({
+          times: inComing,
+        });
+
+        return true;
+      } else {
+
+        return false;
+      }
+    } catch (err) {
+
+      if (err) {
+        console.log(err.response);
+      }
+
+      return false;
+    }
+  };
+
+  async updating(time) {
+    const day = startOfDay(this.state.selectedDate);
+    const nextMonth = addMonths(this.state.current, 1);
+    const numDate = getDate(day);
+    const index = numDate - 1;
+    const nextday = startOfDay(addDays(this.state.selectedDate, 1));
+    const theOne = day.toISOString();
+    const theNext = nextday.toISOString();
+    const timeUpdate = updateTime(this.state.times, time);
+    let res = await axios
+      .put("http://localhost:3005/api/date", {
+        date: theOne,
+        nextdate: theNext,
+        time: timeUpdate,
+      })
+      .catch((err) => console.log(err.response));
+
+    let came = res.data.data.time;
+
+
+    if (isSameMonth(day, this.state.current)) {
+      const tempStateOne = this.state.first;
+      tempStateOne[index].time = came;
+      this.setState({
+        times: timeUpdate,
+        first: tempStateOne,
+      });
+    }
+
+    if (isSameMonth(day, nextMonth)) {
+      const tempStateTwo = this.state.second;
+      tempStateTwo[index].time = came;
+      this.setState({
+        times: timeUpdate,
+        second: tempStateTwo,
+      });
+    }
+  };
+
+  async onDateClick(day) {
+    this.setState({
+      selectedDate: day,
+    });
+
+    const nextMonth = addMonths(this.state.current, 1);
+    if (isSameMonth(day, this.state.current) || isSameMonth(day, nextMonth)) {
+
+      let weGood = await this.getEm(day);
+      if (weGood) {
+        navigate(`/day`);
+
+      } else {
+        this.setState({
+          error: true,
+        })
+
+        await this.delay(3500);
+
+        this.setState({
+          error: false,
+        })
+      }
+    } else {
+      this.setState({
+        error: true,
+      })
+      await this.delay(3500);
+      this.setState({
+        error: false,
+      })
+    }
+
+  };
+
+  nextMonth() {
+    this.setState({
+      currentMonth: addMonths(this.state.currentMonth, 1),
+    }, () => {
+      const sameAsCurrent = this.state.current;
+      const nextMonth = addMonths(this.state.current, 1);
+      let view = {};
+      let error = true;
+      if (isSameMonth(this.state.currentMonth, sameAsCurrent)) {
+        view = this.state.first;
+        error = false;
+      }
+
+      if (isSameMonth(this.state.currentMonth, nextMonth)) {
+        view = this.state.second;
+        error = false;
+      }
+
+      this.setState({
+        initTime: view,
+        error: error,
+      })
+    });
+  };
+
+  prevMonth() {
+    this.setState({
+      currentMonth: subMonths(this.state.currentMonth, 1),
+    }, () => {
+      const sameAsCurrent = this.state.current;
+      const nextMonth = addMonths(this.state.current, 1);
+      let view = {};
+      let error = true;
+
+      if (isSameMonth(this.state.currentMonth, sameAsCurrent)) {
+        view = this.state.first;
+        error = false;
+      }
+
+      if (isSameMonth(this.state.currentMonth, nextMonth)) {
+        view = this.state.second;
+        error = false;
+      }
+
+      this.setState({
+        initTime: view,
+        error: error,
+      })
+
+    });
+  };
+
+  delay(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Router>
+          <Calendar
+            currentMonth={this.state.currentMonth}
+            selectedDate={this.state.selectedDate}
+            times={this.state.times}
+            getEm={this.getEm}
+            dateClick={this.onDateClick}
+            prevMonth={this.prevMonth}
+            nextMonth={this.nextMonth}
+            initTime={this.state.initTime}
+            error={this.state.error}
+            default path="/cal" />
+          <DayChosen
+            selectedDate={this.state.selectedDate}
+            times={this.state.times}
+            update={this.updating}
+            path="/day" />
+          <Cthanks
+            path="/thx" />
+        </Router>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ cal-client/src/Components/DayChosen/DayChosen.js
@@ -0,0 +1,144 @@
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { Link, navigate } from "@reach/router";
+
+import "./DayChosen.css";
+
+const DayChosen = (props) => {
+  const [day] = useState(props.selectedDate);
+  const [times] = useState(props.times);
+  const [verify, setVerify] = useState(false);
+  const [cancSho, setCancSho] = useState(false);
+  const [sTime, setsTime] = useState("");
+  const [counter, setCount] = useState(8);
+  const [yeep, setYeep] = useState(false);
+  const [loaded, setLoaded] = useState([]);
+  const headFormat = "MMMM d yyyy";
+  const weekDay = "EEEE";
+
+  useEffect(() => {
+    setLoaded(require("../../../public/winksC.png"))
+  }, [])
+
+  const update = (time) => {
+    setsTime(time);
+    setVerify(true);
+    veriMessage();
+  };
+
+  const veriMessage = () => {
+    let timeOf = "";
+    if (sTime == "9:00" || sTime == "10:00" || sTime == "11:00")
+      timeOf = "A.M.";
+    else
+      timeOf = "P.M.";
+    return (
+      <div className="vMessage">
+        <h2>Are you sure you would like an appointment for {format(day, headFormat)} @ {sTime} {timeOf}?</h2>
+        <select><option onClick={(e) => yes()}>Yes</option><option onClick={(e) => no()}>No</option></select>
+      </div>
+    )
+  }
+
+  const no = () => {
+    setVerify(false);
+    setsTime("");
+    cancel();
+  }
+
+  const cancel = async () => {
+    setCancSho(true);
+    await delay(2000);
+    setCancSho(false);
+  }
+
+  const yes = () => {
+    props.update(sTime);
+    setVerify(false);
+    timedThx();
+  }
+
+  function delay(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    })
+  }
+
+  async function timedThx() {
+    setYeep(true);
+    await delay(8000);
+    navigate("/thx");
+  }
+
+
+  useEffect(() => {
+    if (yeep) {
+      const timer =
+        counter > 0 && setInterval(() => setCount(counter - 1), 1000);
+      return () => clearInterval(timer);
+    }
+  }, [yeep, counter]);
+
+  const countEl = () => {
+    if (yeep) {
+      return (
+        <div className="count"><h2>Demo: DATABASE UPDATED! Your choice of {sTime} saved.</h2><img src={loaded} alt="Wink Emoji" /><p>Leaving in {counter} seconds...</p></div>
+      )
+    }
+
+    return (
+      <div className="count"><h2>Demo: Working with database..</h2></div>
+    )
+  }
+
+  const onNope = () => {
+    if (cancSho) {
+      return (
+        <div className="canCel"><h2 className="fadeInBottom">Choice cancelled.</h2></div>
+      )
+    }
+    else {
+      return (
+        <div className="unVeri">
+          <h1>I Look Forward To Meeting You!</h1>
+        </div>
+      )
+    }
+  }
+
+  return (
+    <div className="dayChosen">
+      <nav>
+        <Link to="/cal"
+          className="dcLink">Calendar</Link>
+      </nav>
+      <div className="dayHead">
+        <h1>{format(day, headFormat)}</h1>
+        <h1>{format(day, weekDay)}</h1>
+      </div>
+      <div className="temp"></div>
+      <div className="times">
+        <h2 className="tAv">Times Available:</h2>
+        <div className="des">
+          <em>Please select a time that you would be available for a zoom or phone conference</em>
+        </div>
+        <ul>
+          {times.map((time, index) => (
+            <li key={index.toString()} style={{ listStyle: "none" }}>
+              <button
+                className="tButtons"
+                onClick={(e) => update(time)}
+              >
+                {time}
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div className="veri">{verify ? (veriMessage()) : (onNope())}</div>
+      </div>
+      {countEl()}
+    </div>
+  );
+};
+
+export default DayChosen;
